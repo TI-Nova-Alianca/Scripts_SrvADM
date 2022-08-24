c53d8ca88fdcb5819f2bb769bbab410be256814c
Robertkochbr <robert.koch@novaalianca.coop.br>
2022-08-24 11:00:50 -0300

    Scripts SrvADM commit inicial 1

---
+++ Limpa_pasta_publico.ps1
@@ -0,0 +1,14 @@
+﻿
+<# Agenda tarefa no Windows - Selecione e execute:
+
+$nomeScript = 'Limpa_pasta_publico'
+schtasks /create /TN $nomeScript /SC DAILY /ST 00:00 /RI 1400 /DU 24:00  /RU "vinhos-alianca\Administrador" /TR "powershell -file c:\util\scripts\$nomeScript.ps1"
+
+#>
+
+# Limpa arquivos antigos.
+#Get-ChildItem 'd:\documentos\publico\'*.* -Recurse | where -FilterScript {$_.LastWriteTime -le [System.DateTime]::Now.AddMinutes(-30)} | remove-item -Recurse -force
+Get-ChildItem 'C:\ProgramData\SmartSync\logs\'*.* -Recurse | where -FilterScript {$_.LastWriteTime -le [System.DateTime]::Now.AddDays(-30)} | remove-item -Recurse -force
+
+
+
