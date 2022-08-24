3433e77769d24f3c7298869b9a0ad3fe6beb06b4
tialianca <57809637+tialianca@users.noreply.github.com>
2022-08-24 10:13:15 -0300

    Alter_user - commit inicial

---
+++ Alter_User.ps1
@@ -0,0 +1,16 @@
+﻿$FileCSV= Import-CSV -Path C:\Util\scripts\ramais.csv
+Import-Module ActiveDirectory
+
+Foreach ($Txt in $FileCSV) { 
+    
+    $User            =$Txt.Nome
+    $telephoneNumber =$Txt.Telefone
+    $otherTelephone  =$Txt.Celular
+    $mail            =$Txt.E-Mail
+    $department      =Txt.Departamento
+
+    Get-ADUser -Identity $user | Set-ADUser -OfficePhone $telephoneNumber;
+    Get-ADUser -Identity $user | Set-ADUser -Clear otherTelephone;
+    Get-ADUser -Identity $user | Set-ADUser -Add @{otherTelephone=$otherTelephone};
+
+}
