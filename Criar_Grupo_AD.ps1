d3cfb358765015f630ae4b115c2492a5a5f16a83
Robertkochbr <robert.koch@novaalianca.coop.br>
2023-07-09 09:11:43 -0300

    Script Criar_Grupo_AD

--- Criar_Grupo_AD.ps1
+++ Criar_Grupo_AD.ps1
@@ -1,18 +1,20 @@
 ﻿
-$Desc1="Financeiro\Comprovantes_RH"
-#$Desc1="Governanca\Novos POPs\POP-JUR"
-#$Desc1="Governanca\Novos POPs\POP-SUP"
-#$Desc1="Governanca\Novos POPs\POP-CUS"
 
 
-$GrpName   =  "GrpFinCompRH" 
+
+
+
+
+$Desc1="Governanca\Novos POPs\POP-VEN"
+$GrpName   =  "GrpPOP_VEN"
+
 $GrpDesc   =  $Desc1
-$GrpMembros = "andre.oliveira";"franciele.oliveira";"yulli.pereira"
+#$GrpMembros = "andre.oliveira";"franciele.oliveira";"yulli.pereira"
 
 $GrPath    =  "OU=Grupos FileShare,DC=vinhos-alianca,DC=local"
-$GrpMembroDe =  "Grp"+$GrpDesc.Split('\')[0]+"_N1"
+$GrpMembroDe ="GrpNovosPOPs" # "Grp"+$GrpDesc.Split('\')[0]+"_N1"
 
 
 New-ADGroup -GroupScope Global -Name $GrpName -Description $GrpDesc -path $GrPath
-ADD-ADGroupMember $GrpName  -Members $GrpMembros.Split(";")
+#ADD-ADGroupMember $GrpName  -Members $GrpMembros.Split(";")
 ADD-ADGroupMember $GrpMembroDe  -Members $GrpName
